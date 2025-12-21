78f1824004112302445cf7bcc2872690adf59dc4
Cosmic <cosmic@Cosmics-MacBook-Pro.local>
2025-12-21 19:52:27 +0000

    init commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Hello, I'm Life Buddy AI :)</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
